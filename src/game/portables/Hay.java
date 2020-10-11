ecad75b97ffd3201dfc6ee1f86b70f65c2be7ada
nchu0010 <nchu0010@student.monash.edu>
2020-10-12 01:00:59 +1100

    Merge branch 'master' into 'nix-chu/implement_hay'
    # Conflicts:
    #   src/game/ground/Dirt.java

---
+++ src/game/portables/Hay.java
@@ -0,0 +1,30 @@
+package game.portables;
+
+import game.actors.Dinosaur;
+import game.actors.Stegosaur;
+
+public class Hay extends Food {
+    static final int COST = 20;
+    static final int FILL = 20;
+    static final int POINTS_WHEN_HAY_FED = 10;
+
+    public Hay(String name, char displayChar) {
+        super(name, displayChar);
+    }
+
+    public int getCost() { return COST; }
+
+    public void feed(Dinosaur dinosaur) {
+        if (dinosaur instanceof Stegosaur) {
+            try {
+                dinosaur.increaseHunger(FILL);
+            }
+            catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Only Stegosaurs can eat hay.");
+        }
+    }
+}
