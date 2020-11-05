e6252d9ea1f23fc3188d26c9a89d70ba3585307f
av0z <aldenhvong@gmail.com>
2020-11-06 10:52:31 +1100

    Format and add Javadoc for DrinkAction and EatAction

--- src/game/actions/DrinkAction.java
+++ src/game/actions/DrinkAction.java
@@ -3,23 +3,33 @@ package game.actions;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
-import edu.monash.fit2099.engine.Ground;
 import game.actors.Dinosaur;
 import game.ground.Water;
 
-// TODO: Javadoc
+/**
+ * Special Action for a Dinosaur to drink. Extends Action.
+ *
+ * @author Nicholas Chua and Alden Vong
+ */
 public class DrinkAction extends Action {
-    private Ground target;
+    private Water target;
 
-    public DrinkAction(Ground ground) {
-        this.target = ground;
+    /**
+     * Constructor for when a Dinosaur drinks from a Water object.
+     *
+     * @param water - Water to drink from
+     */
+    public DrinkAction(Water water) {
+        this.target = water;
     }
+
+    @Override
     public String execute(Actor actor, GameMap map) {
         Dinosaur dinosaur = (Dinosaur) actor;
-        Water water = (Water) this.target;
-        dinosaur.increaseThirstPoints(water.getFill());
+        dinosaur.increaseThirstPoints(this.target.getFill());
         return menuDescription(actor);
     }
+    @Override
     public String menuDescription(Actor actor) {
         return actor + " drank water ";
     }
--- src/game/actions/EatAction.java
+++ src/game/actions/EatAction.java
@@ -5,38 +5,59 @@ import game.actors.Dinosaur;
 import game.ground.Dirt;
 import game.items.WildEdible;
 
-// TODO: Javadoc
+/**
+ * Special Action for a Dinosaur to eat. Extends Action
+ *
+ * @author Nicholas Chua and Alden Vong
+ */
 public class EatAction extends Action {
     private Ground groundTarget;
     private Item itemTarget;
 
+    /**
+     * Constructor for when a Dinosaur eats from a Ground object. At the moment, this only applies
+     * to Dirt objects with grass.
+     *
+     * @param ground - Ground to eat from
+     */
     public EatAction(Ground ground) {
         this.groundTarget = ground;
     }
+
+    /**
+     * Constructor for when a Dinosaur eats an Item object (lying on the ground). At the moment, this
+     * only applied to corpses and eggs of other species.
+     *
+     * @param item - Item to eat
+     */
     public EatAction(Item item) {
         this.itemTarget = item;
     }
+
+    @Override
     public String execute(Actor actor, GameMap map) {
         Dinosaur dinosaur = (Dinosaur) actor;
-        if (groundTarget != null) {
+
+        if (this.groundTarget != null) {
             Dirt dirt = (Dirt) this.groundTarget;
             dinosaur.increaseHungerPoints(dirt.getFill());
             dirt.removeGrass();
-            return menuDescription(dinosaur);
         }
         else {
-            WildEdible edible = (WildEdible) itemTarget;
+            WildEdible edible = (WildEdible) this.itemTarget;
             dinosaur.increaseHungerPoints(edible.getFill());
             map.locationOf(actor).removeItem((Item) edible);
-            return menuDescription(dinosaur);
         }
+        return menuDescription(dinosaur);
     }
+
+    @Override
     public String menuDescription(Actor actor) {
-        if (groundTarget != null) {
+        if (this.groundTarget != null) {
             return actor + " eats grass";
         }
         else {
-            return actor + " eats " + itemTarget;
+            return actor + " eats " + this.itemTarget;
         }
     }
 }
